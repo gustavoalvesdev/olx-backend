7afda3e79f9516011c47851e8d5cc5cca5d17c5d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-04-21 21:19:30 -0300

    Listando Estados e Categorias

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\StatesController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -8,19 +9,22 @@ Route::get('/ping', function(): JsonResponse {
     return response()->json(['Pong' => true]);
 });
 
+Route::get('/states', [StatesController::class, 'index']);
+Route::get('/categories', [CategoriesController::class, 'index']);
+
 /**
  * Rota de Utilidade
  * [x] - /ping - Responde com Pong
- * 
+ *
  *  - Rotas de Autenticação * Autenticação via TOKEN
  * [ ] - /user/signin -- Login
  * [ ] - /user/signup -- Registro do usuário
  * [ ] - /user/me --- Informações do usuário logado
- * 
+ *
  * - Rotas de configuração geral
- * [ ] - /states - Listar os estados
- * [ ] - /categories - Listar as categorias do sistema
- * 
+ * [x] - /states - Listar os estados
+ * [x] - /categories - Listar as categorias do sistema
+ *
  * - Rotas de Advertises
  * [ ] - /ad/list - Listar todos os anúncios
  * [ ] - /ad/:id  - Dados de um anúncio único
@@ -28,5 +32,5 @@ Route::get('/ping', function(): JsonResponse {
  * [ ] - /ad/:id(PUT) - Alterar um anúncio publicado
  * [ ] - /ad/:id(delete) - Deletar um anúncio
  * [ ] - /ad/:id/:image (Deletar a imagem de um anúncio)
- * 
+ *
  */
